cab70fb793280daa7a5e81a29ad8aecce32763f0
Jonghwa Woo <wjh0630@gmail.com>
2016-12-27 18:08:03 +0900

    List all records

--- app/views.py
+++ app/views.py
@@ -2,8 +2,14 @@ from flask import render_template
 from . import app
 
 @app.route('/')
-def hello():
-    return "Hello World! hoo"
+def show_all():
+    with app.app_context():
+        eat = Category.query.filter_by(name='처먹').first()
+        items = eat.items.all()
+        records = [item.records.all() for item in items]
+        records = [record for sublist in records for record in sublist]
+
+        return render_template('show.html', records=records)
 
 
 @app.route('/<name>')
