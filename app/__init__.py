54ab38029f306981f560d2ba3394496d35d29b10
Jonghwa Woo <wjh0630@gmail.com>
2017-01-02 22:37:04 +0900

    Refine config structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from .config import config
+from config import config
 import os
 
 db = SQLAlchemy()
 
 
 def create_app(config_name):
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config[config_name])
+    app.config.from_pyfile('config.py')
     db.init_app(app)
     return app
 
